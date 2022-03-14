2d39812cb540ef93278b2bc5cbff0a03c626f529
laurabjn <92058813+laurabjn@users.noreply.github.com>
2022-03-14 13:40:56 +0100

    feat footer

---
+++ caalliope_front/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+const Footer = () => {
+    return (
+        <footer class="bg-light text-center text-lg-start">
+        <div class="container p-4">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Informations utiles</h5>
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="/Contact/" class="text-dark">Nous contacter</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="text-center p-3">
+            © 2022 Copyright:
+            <a class="text-dark" href="#"> Caalliope</a>
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;
