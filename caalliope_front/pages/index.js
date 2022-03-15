9337df748bfe6a3f45e616844ee2f7ec9d9e24d0
laurabjn <92058813+laurabjn@users.noreply.github.com>
2022-03-15 10:46:25 +0100

    feat create footer + css footer

--- caalliope_front/pages/index.js
+++ caalliope_front/pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.footer}>
+      <Head>
+      </Head>
+      <main className={styles.main}>
+      </main>
+      <footer className={styles.footer}>
         < Footer />
-      </div>
+      </footer>
     </div>
   )
 }
