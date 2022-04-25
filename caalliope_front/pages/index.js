e36e9211b59e3d00e3983feba817057b5547deea
laurabjn <92058813+laurabjn@users.noreply.github.com>
2022-04-25 23:18:35 +0200

    feat routing

--- caalliope_front/pages/index.js
+++ caalliope_front/pages/index.js
@@ -1,11 +1,31 @@
 import HomePage from './homePage';
+import ConnectPage from './connectPage';
+import InscriptionPage from './inscriptionPage';
+import UserPage from './userPage';
+import BookInfoPage from './BookInfoPage';
+import Bibliotheque from './BrowsePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams,
+} from 'react-router-dom';
 import styles from '../styles/Home.module.css';
 
 const Home = () => {
+  const match = useRouteMatch();
   return (
-    <main className={styles.container}>
-      < HomePage />
-    </main>
+    <Router>
+      <Switch>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/connection" element={<ConnectPage/>}/>
+        <Route path="/inscription" element={<InscriptionPage/>}/>
+        <Route path="/user/:userId" element={<UserPage/>}/>
+        <Route path="/browse" element={<Bibliotheque/>}/>
+        <Route path="/browse/:bookId" element={<BookInfoPage/>}/>
+      </Switch>
+    </Router>
   );
 }
 
