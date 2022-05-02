7da246995a1468877d69194fae3f3bc4d9d47759
bascor1207 <bascor1207@gmail.com>
2022-05-02 10:35:14 +0200

    fix : fix api call

--- caalliope_front/components/HomePage/AudioBook.js
+++ caalliope_front/components/HomePage/AudioBook.js
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import BookItem from './BookItem';
+import ReactAudioPlayer from 'react-audio-player';
+import styles from './AudioBook.module.css';
 
 const AudioBook = () => {
-  const [data, setData] = useState("");
+  const [book, setBook] = useState("");
   const [error, setError] = useState("");
   
     useEffect(() => {
-      fetch("https:/openlibrary.org/api/volumes/brief/ibsn/9780807286005.json")
+      fetch("https://openlibrary.org/isbn/9780140328721.json")
         .then((response) => response.json())
-        .then((data) => setData(data))
+        .then((book) => setBook(book))
         .catch(setError);
     }, []);
 
@@ -16,17 +18,32 @@ const AudioBook = () => {
       return <pre>{JSON.stringify(error, null, 2)}</pre>
     }
 
-    if (!data) {
+    if (!book) {
       return null;
-    }
-    return (
-      <div className="bookitem">
-        {data.docs.map((book, i) => (
-          <BookItem key={i} books={book}/>
-        ))}
-      </div>
-    );
-    
-};
+    }      
+      const handleClick = () => {
+        <ReactAudioPlayer/>
+        };
+        return(
+          <div className="card mb-3">
+              <div className="row g-0">
+                  <div className="col-md-4">
+                      <img 
+                        src={book.covers} 
+                        className={styles.cover} 
+                        alt="..."/>
+                  </div>
+                  <div className="col-md-8">
+                      <div className="card-body">
+                          <h5 className="card-title">{book.title}</h5>
+                          <button className={styles.audio} onClick={handleClick}>
+                          {"Ecouter l'extrait audio"}
+                          </button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      );
+  }
   
 export default AudioBook;
